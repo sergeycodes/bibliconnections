074dedd65509eb9256f043a1bf7ead556bb1f201
Sergey Barzul <sbarzul@uw.edu>
2024-02-12 21:26:35 -0800

    added an onclick animation to the buttons

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let selectedLables = [];
+let numberOfSelectedLabels = 0;
 
 function selectButton(label) {
     let button = document.getElementById(label);
-    button.style.color = "red";
+    console.log("BG COLOR " + button.style.background);
+    
+    if (numberOfSelectedLabels < 4 && (button.style.background === "rgb(239, 239, 230)" || button.style.background === "")) {
+        button.style.background = "#5A594E";
+        button.style.color = "#FFFFFF"
+        numberOfSelectedLabels++;
+    } else if( button.style.background === "rgb(90, 89, 78)"){
+        button.style.background = "rgb(239, 239, 230)";
+        button.style.color = "#000000"
+        numberOfSelectedLabels--;
+    }
+
+    console.log("Number of selected labels: " + numberOfSelectedLabels);
+
     console.log("Button clicked " + label);
 }
